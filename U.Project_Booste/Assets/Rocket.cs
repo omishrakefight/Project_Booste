8da091d2792a19c42a8d55abf8c5859f655d0e28
omishrakefight <omishrakefight@live.com>
2018-02-08 00:44:22 -0500

    ROCKET FLIES
    The rocket is colored and now flies up.

---
+++ U.Project_Booste/Assets/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        // Makes it reference the Rigidbody attached to the rocket ship.
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        // Can thrust while Pivoting.
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        // Can only turn 1 direction at a time.
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Left was pushed");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Right was pushed");
+        }
+    }
+}
